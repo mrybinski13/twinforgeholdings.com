b58e9732c12127b2f6cf1ad23782f5701e16c12a
mrybinski13 <mrybinski13@gmail.com>
2026-08-24 09:35:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{const b=document.querySelector('.menu');const n=document.querySelector('.nav nav');if(b&&n)b.addEventListener('click',()=>n.classList.toggle('open'));});
